a2ddce678bfc6b241fed6c6f7cf5bbc3ebfd6a88
Santiago Lezica <salezica@gmail.com>
2021-12-05 20:05:17 -0300

    Simplify main.go a bit

--- src/main.go
+++ src/main.go
@@ -10,22 +10,18 @@ import (
 func main() {
 	cli := ParseCli()
 
-	doTheThing(cli.splitter, cli.joiner, cli.selectors)
-}
-
-func doTheThing(splitter *Splitter, joiner *Joiner, selectors []*Selector) {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		fields := splitter.Split(line)
+		fields := cli.splitter.Split(line)
 
 		var selectedFields []string
 
-		for _, selector := range selectors {
+		for _, selector := range cli.selectors {
 			selectedFields = append(selectedFields, selector.Select(fields)...)
 		}
 
-		fmt.Println(joiner.Join(selectedFields))
+		fmt.Println(cli.joiner.Join(selectedFields))
 	}
 }
